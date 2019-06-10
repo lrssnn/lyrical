c2ba34f7c96ce8a351ed59c0945deddc649711bd
Niklas Larsson <niklaslarsson95@gmail.com>
2019-06-10 19:07:02 +1000

    Big commit of words
    Words from raw dictionary in "dict" have been sorted into files by word
    form (noun, verb etc) however there are lots of little issues which I
    will handle by hand

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def generate_speech_from_text(input):
 
     play_sound("output.mp3")
 
+
 def play_sound(filename):
     import pygame
     import time
@@ -31,11 +32,12 @@ def play_sound(filename):
     pygame.mixer.music.play()
 
     print('Playing...')
-    while(pygame.mixer.music.get_busy()):
+    while pygame.mixer.music.get_busy():
         print(pygame.mixer.music.get_pos() / 1000)
         time.sleep(1)
         #print('.', end="")
     print("Complete")
+
 
 def generate_sentence():
     # I suppose we would like to choose:
@@ -53,6 +55,7 @@ def generate_sentence():
     predicate = generate_predicate()
 
     return subject.to_string() + ' ' + predicate.to_string() + '.'
+
 
 # From here I am stubbing out
 class TreeNode:
@@ -75,7 +78,10 @@ class TreeNode:
             return result.strip(' ')
 
 
-
+# I am thinking about how to generate sentences with some coherence...
+# I think we will need to choose a tense that the entire sentence lives under,
+# Then generate our subject and predicate. We should also consider the placement
+# of subject and predicate. I suppose
 def generate_subject():
     return TreeNode("Leaf", "I")
 
@@ -95,7 +101,83 @@ def generate_predicate():
 
     return root
 
+
+def process_dictionary():
+    delimiter = ">>|<<"
+    types = {}
+    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print("Processing " + letter)
+        with open('dict\\OPTED v0.03 Letter '+letter+'.html', 'r') as dict:
+            #for i in range(20):
+            for line_raw in dict:
+
+                #line = dict.readline().strip()
+                line = line_raw.strip()
+                if not line.startswith("<p>"):
+                    continue
+
+                #print(line)
+
+                line = line.replace('<p>', '').replace("<b>", "").replace("</p>", "").replace("</b> ", delimiter)
+                line = line.replace("(<i>", "").replace("</i>) ", delimiter)
+
+                #print(line)
+                parts = line.split(delimiter)
+                if len(parts) != 3:
+                    print("Error: " + line_raw)
+                    continue
+
+                type = parts[1]
+                if len(type) == 0:
+                    continue
+                type_parts = type.split(" &amp; ")
+                for part in type_parts:
+                    if part not in types:
+                        types[part] = part
+
+
+    junk = open("processed\\JUNK.txt", "w+")
+    for type in types.keys():
+        print(type)
+        try:
+            file = open("processed\\ProcessedDict-" + type + ".txt", "w+")
+            types[type] = file
+        except:
+            print("oop:" + type)
+            types[type] = junk
+
+    # Naughty, now we're doing it for real
+    for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+        print("Processing " + letter)
+        with open('dict\\OPTED v0.03 Letter '+letter+'.html', 'r') as dict:
+            for line_raw in dict:
+
+                line = line_raw.strip()
+                if not line.startswith("<p>"):
+                    continue
+
+                line = line.replace('<p>', '').replace("<b>", "").replace("</p>", "").replace("</b> ", delimiter)
+                line = line.replace("(<i>", "").replace("</i>) ", delimiter)
+
+                parts = line.split(delimiter)
+                if len(parts) != 3:
+                    print("Error: " + line_raw)
+                    continue
+
+                type = parts[1]
+                if len(type) == 0:
+                    continue
+                type_parts = type.split(" &amp; ")
+                for type in type_parts:
+                    types[type].writelines(line + "\n")
+
+    for key in types.keys():
+        types[key].close()
+
+
+
 if __name__ == '__main__':
-    text = "This is a test"
-    print(generate_sentence())
+    #print(generate_sentence())
     # generate_speech_from_text(text)
+    process_dictionary()
+
